e7c4292cd51ece2c3e480d85502ced60f38d8962
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2022-10-19 18:56:48 +0200

    "Código creado"

---
+++ concurso.py
@@ -0,0 +1,11 @@
+respuesta = " s";
+print ("¿Cual es la capital de Mongolia?");
+
+print ("a) Ulán Bator");
+print ("b) Daca");
+print ("c) Nicosia");
+
+if (respuesta == 'a'):
+    print("Respuesta correcta");
+else:
+    print("Respuesta incorrecta");
